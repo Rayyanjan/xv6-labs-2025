147a998e10dd180a671d3d72d9b7a47a92da3d97
Muhammad Rayyan Jan <98212871+Rayyanjan@users.noreply.github.com>
2025-11-11 09:35:00 +0500

    Update kalloc.c

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -10,17 +10,12 @@
 #include "defs.h"
 #include "memlayout.h"
 
-// ADDED BY SAFEGUARD
 static void *super_free_list[N_SUPERPAGES];
 static int super_free_cnt = 0;
 
-
-// ADDED BY SAFEGUARD, slight name change to avoid conflict
 void freerange(void *vstart, void *vend);
-// void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
-                   // defined by kernel.ld.
 
 struct run {
   struct run *next;
@@ -38,7 +33,6 @@ kinit()
   freerange(end, (void*)PHYSTOP);
 }
 
-// ADDED BY SAFEGUARD
 void
 freerange(void *vstart, void *vend)
 {
@@ -56,16 +50,6 @@ freerange(void *vstart, void *vend)
   }
 }
 
-// void
-// freerange(void *pa_start, void *pa_end)
-// {
-//   char *p;
-//   p = (char*)PGROUNDUP((uint64)pa_start);
-//   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-//     kfree(p);
-// }
-
-// ADDED BY SAFEGUARD
 void *
 superalloc(void)
 {
@@ -74,7 +58,6 @@ superalloc(void)
   return super_free_list[--super_free_cnt];
 }
 
-// ADDED BY SAFEGUARD
 void
 superfree(void *pa)
 {
@@ -84,10 +67,6 @@ superfree(void *pa)
   super_free_list[super_free_cnt++] = pa;
 }
 
-// Free the page of physical memory pointed at by pa,
-// which normally should have been returned by a
-// call to kalloc().  (The exception is when
-// initializing the allocator; see kinit above.)
 void
 kfree(void *pa)
 {
@@ -96,7 +75,6 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
   r = (struct run*)pa;
